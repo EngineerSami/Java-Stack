2a2cd43cc9be2ca975c2ecc2e2dc3679c1ef444f
Sami <sami.daraghmeh1306@gmail.com>
2024-12-04 15:07:48 +0200

    JavaCafe

--- JavaCafe/JavaCafe.java
+++ JavaCafe/JavaCafe.java
@@ -1,19 +1,35 @@
-package JavaCafe;
+
+import java.util.ArrayList;
 
 public class JavaCafe {
-    for(int i=0 ; i<=10 ; i++){
-        if (i<=5) {
-           System.out.println(i); 
+    public int getStreakGoal() {
+        int sum = 0;
+        for (int i = 1; i <= 10; i++) {
+            sum += i;
         }
-        elseif (i>=5); {
-                    System.out.println(i);
-                } else {
-                    System.out.println(i);
-                } 
-                
-            }
-            private void elseif(boolean b) {
-                // TODO Auto-generated method stub
-                throw new UnsupportedOperationException("Unimplemented method 'elseif'");
-            }
+        return sum;
+    }
+
+    public double getOrderTotal(double[] prices) {
+        double total = 0;
+        for (double price : prices) {
+            total += price;
+        }
+        return total;
+    }
+
+    public void displayMenu(ArrayList<String> menuItems) {
+        for (int i = 0; i < menuItems.size(); i++) {
+            System.out.println(i + " " + menuItems.get(i));
+        }
+    }
+
+    public void addCustomer(ArrayList<String> customers) {
+        System.out.println("Enter your name please");
+        String username = System.console().readLine();
+        System.out.println("Hello, " + username + "!");
+        System.out.println("There are " + customers.size() + " people in front of you.");
+        customers.add(username);
+        System.out.println(customers);
+    }
 }
