e13582404ac61bee3c7d8d7a717ca95f7693e8c7
Sami <sami.daraghmeh1306@gmail.com>
2024-12-10 13:30:44 +0200

    QueryParameters

---
+++ queryparameters/src/main/java/com/axsos/query/queryparameters/QueryparametersApplication.java
@@ -0,0 +1,13 @@
+package com.axsos.query.queryparameters;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryparametersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryparametersApplication.class, args);
+	}
+
+}
---
+++ queryparameters/src/test/java/com/axsos/query/queryparameters/QueryparametersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axsos.query.queryparameters;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryparametersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
